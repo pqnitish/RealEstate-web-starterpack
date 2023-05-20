07f146e5a115bce447552296d0e1ccb945eeed0b
nitish <pqnitish2020@gmail.com>
2023-05-20 12:28:29 +0530

    completed

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get Started with Homyz</span>
+          <span className="secondaryText">Subscribe and find attractive price quotes from us.
+          <br/>
+          Find your residence soon
+          </span>
+          <button className="button">
+            <a href="pqnitish2020@gmail.com">Get Started</a>
+
+          </button>
+          
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetStarted;
